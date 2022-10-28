a169e97211543301a4fb4493116775fefebceeef
lydenn <lyden@go24.sg>
2022-10-28 11:45:32 +0700

    init package

--- LICENSE.md
+++ LICENSE.md
@@ -1,6 +1,6 @@
 The MIT License (MIT)
 
-Copyright (c) :vendor_name <author@domain.com>
+Copyright (c) lydenn <lyden@go24.sg>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- src/PunkhazardServiceProvider.php
+++ src/PunkhazardServiceProvider.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace VendorName\Skeleton;
+namespace Lydenn\Punkhazard;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use VendorName\Skeleton\Commands\SkeletonCommand;
+use Lydenn\Punkhazard\Commands\PunkhazardCommand;
 
-class SkeletonServiceProvider extends PackageServiceProvider
+class PunkhazardServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
@@ -16,10 +16,10 @@ class SkeletonServiceProvider extends PackageServiceProvider
          * More info: https://github.com/spatie/laravel-package-tools
          */
         $package
-            ->name('skeleton')
+            ->name('punkhazard')
             ->hasConfigFile()
             ->hasViews()
-            ->hasMigration('create_skeleton_table')
-            ->hasCommand(SkeletonCommand::class);
+            ->hasMigration('create_punkhazard_table')
+            ->hasCommand(PunkhazardCommand::class);
     }
 }
